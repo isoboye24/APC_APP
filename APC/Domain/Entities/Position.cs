d410cff00189780500882a47fc21b0e35a59e48a
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-02 22:05:54 +0100

    Refactored Position Entity

---
+++ APC/Domain/Entities/Position.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Entities
+{
+    public class Position
+    {
+        public int PositionId { get; private set; }
+        public string PositionName { get; private set; }
+
+        public Position(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Position name cannot be empty");
+
+            PositionName = name.Trim();
+        }
+
+        public static Position Rehydrate(int id, string name)
+        {
+            return new Position(name)
+            {
+                PositionId = id,
+            };
+        }
+
+        public void UpdateName(string newName)
+        {
+            if (string.IsNullOrWhiteSpace(newName))
+                throw new ArgumentException("Position name cannot be empty");
+
+            PositionName = newName.Trim();
+        }
+
+        public void SetId(int id)
+        {
+            PositionId = id;
+        }
+    }
+}
