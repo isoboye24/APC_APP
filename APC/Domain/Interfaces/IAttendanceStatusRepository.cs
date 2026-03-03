07f52dde9f02c9e52c1b095f4180f994bd004dd6
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-03 16:12:06 +0100

    Created Attendance status repository

---
+++ APC/Domain/Interfaces/IAttendanceStatusRepository.cs
@@ -0,0 +1,15 @@
+﻿using APC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Interfaces
+{
+    public interface IAttendanceStatusRepository
+    {
+        List<AttendanceStatus> GetAll();
+        AttendanceStatus GetById(int id);
+    }
+}
---
+++ APC/Infrastructure/Repositories/AttendanceStatusRepository.cs
@@ -0,0 +1,44 @@
+﻿using APC.DAL;
+using APC.Domain.Entities;
+using APC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Infrastructure.Repositories
+{
+    public class AttendanceStatusRepository : IAttendanceStatusRepository
+    {
+        private readonly APCEntities _db;
+        public AttendanceStatusRepository(APCEntities db)
+        {
+            _db = db;
+        }
+
+        public List<AttendanceStatus> GetAll()
+        {
+            var data = _db.ATTENDANCE_STATUS
+                .OrderBy(x => x.attendanceStatusID)
+                .ToList();
+
+            return data
+                .Select(x => AttendanceStatus.Rehydrate(
+                    x.attendanceStatusID,
+                    x.attendanceStatus
+                ))
+                .ToList();
+        }
+
+        public AttendanceStatus GetById(int id)
+        {
+            var entity = _db.ATTENDANCE_STATUS.FirstOrDefault(x => x.attendanceStatusID == id);
+            if (entity == null) return null;
+
+            var status = new AttendanceStatus(entity.attendanceStatus);
+            status.SetId(entity.attendanceStatusID);
+            return status;
+        }
+    }
+}
