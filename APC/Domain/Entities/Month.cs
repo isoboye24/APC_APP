2bc3266c66ec0731619e1e28d23c5eb054ef3ee2
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-03 15:45:56 +0100

    Created Month repository

--- APC/Domain/Entities/Month.cs
+++ APC/Domain/Entities/Month.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using APC.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,11 @@ namespace APC.Domain.Entities
     public class Month
     {
         public int MonthId { get; private set; }
-        public string Name { get; private set; }
+        public string MonthName { get; private set; }
 
         public Month(string name)
         {
-            Name = name;
+            MonthName = name;
         }
 
         public static Month Rehydrate(int id, string name)
@@ -22,6 +23,11 @@ namespace APC.Domain.Entities
             {
                 MonthId = id
             };
+        }
+
+        public void SetId(int id)
+        {
+            MonthId = id;
         }
     }
 }
---
+++ APC/Infrastructure/Repositories/MonthRepository.cs
@@ -0,0 +1,43 @@
+﻿using APC.DAL;
+using APC.Domain.Entities;
+using APC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Infrastructure.Repositories
+{
+    public class MonthRepository : IMonthRepository
+    {
+        private readonly APCEntities _db;
+        public MonthRepository(APCEntities db)
+        {
+            _db = db;
+        }
+        public List<Month> GetAll()
+        {
+            var data = _db.MONTH
+                .OrderBy(x => x.monthID)
+                .ToList();
+
+            return data
+                .Select(x => Month.Rehydrate(
+                    x.monthID,
+                    x.monthName
+                ))
+                .ToList();
+        }
+
+        public Month GetById(int id)
+        {
+            var entity = _db.MONTH.FirstOrDefault(x => x.monthID == id);
+            if (entity == null) return null;
+
+            var month = new Month(entity.monthName);
+            month.SetId(entity.monthID);
+            return month;
+        }
+    }
+}
