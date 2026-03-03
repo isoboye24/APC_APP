8de0e52891eda01a5e6e3b5fab39624e89454d25
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-03 14:00:38 +0100

    Created Nationality repository

---
+++ APC/Domain/Interfaces/INationalityRepository.cs
@@ -0,0 +1,22 @@
+﻿using APC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Interfaces
+{
+    public interface INationalityRepository
+    {
+        List<Nationality> GetAll();
+        Nationality GetById(int id);
+        bool Insert(Nationality nationality);
+        bool Update(Nationality nationality);
+        bool Delete(int id);
+        bool GetBack(int id);
+        bool PermanentDelete(int id);
+        bool Exists(string name);
+        int Count();
+    }
+}
---
+++ APC/Infrastructure/Repositories/NationalityRepository.cs
@@ -0,0 +1,106 @@
+﻿using APC.DAL;
+using APC.Domain.Entities;
+using APC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace APC.Infrastructure.Repositories
+{
+    public class NationalityRepository : INationalityRepository
+    {
+        private readonly APCEntities _db;
+        public NationalityRepository(APCEntities db)
+        {
+            _db = db;
+        }
+
+        public int Count()
+        {
+            return _db.NATIONALITY.Count(x => !x.isDeleted);
+        }
+
+        public bool Delete(int id)
+        {
+            var entity = _db.NATIONALITY.First(x => x.nationalityID == id);
+            entity.isDeleted = true;
+            entity.deletedDate = DateTime.Today;
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool Exists(string name)
+        {
+            return _db.NATIONALITY.Any(x => !x.isDeleted && x.nationality1 == name);
+        }
+
+        public List<Nationality> GetAll()
+        {
+            var data = _db.NATIONALITY
+                .Where(x => !x.isDeleted)
+                .OrderBy(x => x.nationality1)
+                .ToList();
+
+            return data
+                .Select(x => Nationality.Rehydrate(
+                    x.nationalityID,
+                    x.nationality1
+                ))
+                .ToList();
+        }
+
+        public bool GetBack(int id)
+        {
+            var entity = _db.NATIONALITY.First(x => x.nationalityID == id);
+            entity.isDeleted = false;
+            entity.deletedDate = null;
+            _db.SaveChanges();
+            return true;
+        }
+
+        public Nationality GetById(int id)
+        {
+            var entity = _db.NATIONALITY.FirstOrDefault(x => x.nationalityID == id && !x.isDeleted);
+            if (entity == null) return null;
+
+            var nationality = new Nationality(entity.nationality1);
+            nationality.SetId(entity.nationalityID);
+            return nationality;
+        }
+
+        public bool Insert(Nationality nationality)
+        {
+            _db.NATIONALITY.Add(new NATIONALITY
+            {
+                nationality1 = nationality.NationalityName
+            });
+
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool PermanentDelete(int id)
+        {
+            var entity = _db.NATIONALITY.FirstOrDefault(x => x.nationalityID == id);
+
+            if (entity == null)
+                return false;
+
+            _db.NATIONALITY.Remove(entity);
+            _db.SaveChanges();
+
+            return true;
+        }
+
+        public bool Update(Nationality nationality)
+        {
+            var entity = _db.NATIONALITY.First(x => x.nationalityID == nationality.NationalityId);
+            entity.nationality1 = nationality.NationalityName;
+            _db.SaveChanges();
+            return true;
+        }
+    }
+}
