8be11520941bc22ee1aca7fb1c1fb119c167cc53
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-03 16:07:41 +0100

    Created attendance status entity

---
+++ APC/Domain/Entities/AttendanceStatus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Entities
+{
+    public class AttendanceStatus
+    {
+        public int AttendanceStatusId { get; private set; }
+        public string AttendanceStatusName { get; private set; }
+
+        public AttendanceStatus(string name)
+        {
+            AttendanceStatusName = name;
+        }
+
+        public static AttendanceStatus Rehydrate(int id, string name)
+        {
+            return new AttendanceStatus(name)
+            {
+                AttendanceStatusId = id
+            };
+        }
+
+        public void SetId(int id)
+        {
+            AttendanceStatusId = id;
+        }
+    }
+}
