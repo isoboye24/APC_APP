a0d88d41d6ed06beb9c9b03386107acf0ea4c391
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-03 16:26:05 +0100

    Created membership status entity

---
+++ APC/Domain/Entities/MembershipStatus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Entities
+{
+    public class MembershipStatus
+    {
+        public int MembershipStatusId { get; private set; }
+        public string MembershipStatusName { get; private set; }
+
+        public MembershipStatus(string name)
+        {
+            MembershipStatusName = name;
+        }
+
+        public static MembershipStatus Rehydrate(int id, string name)
+        {
+            return new MembershipStatus(name)
+            {
+                MembershipStatusId = id
+            };
+        }
+
+        public void SetId(int id)
+        {
+            MembershipStatusId = id;
+        }
+    }
+}
