40a0e829ba8bd8e904db0a471b15b35a9b38aa94
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-03 14:25:02 +0100

    Created Profession entity

---
+++ APC/Domain/Entities/Profession.cs
@@ -0,0 +1,45 @@
+﻿using APC.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Entities
+{
+    public class Profession
+    {
+        public int ProfessionId { get; private set; }
+        public string ProfessionName { get; private set; }
+
+
+        public Profession(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Profession name cannot be empty");
+
+            ProfessionName = name.Trim();
+        }
+
+        public static Profession Rehydrate(int id, string name)
+        {
+            return new Profession(name)
+            {
+                ProfessionId = id,
+            };
+        }
+
+        public void UpdateName(string newName)
+        {
+            if (string.IsNullOrWhiteSpace(newName))
+                throw new ArgumentException("Profession name cannot be empty");
+
+            ProfessionName = newName.Trim();
+        }
+
+        public void SetId(int id)
+        {
+            ProfessionId = id;
+        }
+    }
+}
