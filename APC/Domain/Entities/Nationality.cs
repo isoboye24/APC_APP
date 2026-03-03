077158452b10d844c092fa8b23cbc2a86247e4b1
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-03 13:50:21 +0100

    Created Nationality entity

---
+++ APC/Domain/Entities/Nationality.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Entities
+{
+    public class Nationality
+    {
+        public int NationalityId { get; private set; }
+        public string NationalityName { get; private set; }
+
+
+        public Nationality(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Nationality name cannot be empty");
+
+            NationalityName = name.Trim();
+        }
+
+        public static Nationality Rehydrate(int id, string name)
+        {
+            return new Nationality(name)
+            {
+                NationalityId = id,
+            };
+        }
+
+        public void UpdateName(string newName)
+        {
+            if (string.IsNullOrWhiteSpace(newName))
+                throw new ArgumentException("Nationality name cannot be empty");
+
+            NationalityName = newName.Trim();
+        }
+
+        public void SetId(int id)
+        {
+            NationalityId = id;
+        }
+    }
+}
