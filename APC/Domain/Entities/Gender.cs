5d8cd8960652421559b40cacbb6ee1c3aa8c266d
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-03 15:53:22 +0100

    Created Gender entity

---
+++ APC/Domain/Entities/Gender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Entities
+{
+    public class Gender
+    {
+
+        public int GenderId { get; private set; }
+        public string GenderName { get; private set; }
+
+        public Gender(string name)
+        {
+            GenderName = name;
+        }
+
+        public static Gender Rehydrate(int id, string name)
+        {
+            return new Gender(name)
+            {
+                GenderId = id
+            };
+        }
+
+        public void SetId(int id)
+        {
+            GenderId = id;
+        }
+    }
+}
