6255966cf3395d490094888f53edb4eadf9ae683
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-03 14:51:18 +0100

    Created the repository for the profession

---
+++ APC/Domain/Interfaces/IProfessionRepository.cs
@@ -0,0 +1,22 @@
+﻿using APC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Interfaces
+{
+    public interface IProfessionRepository
+    {
+        List<Profession> GetAll();
+        Profession GetById(int id);
+        bool Insert(Profession profession);
+        bool Update(Profession profession);
+        bool Delete(int id);
+        bool GetBack(int id);
+        bool PermanentDelete(int id);
+        bool Exists(string name);
+        int Count();
+    }
+}
---
+++ APC/Infrastructure/Repositories/ProfessionRepository.cs
@@ -0,0 +1,105 @@
+﻿using APC.DAL;
+using APC.Domain.Entities;
+using APC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Infrastructure.Repositories
+{
+    public class ProfessionRepository : IProfessionRepository
+    {
+        private readonly APCEntities _db;
+        public ProfessionRepository(APCEntities db)
+        {
+            _db = db;
+        }
+
+        public int Count()
+        {
+            return _db.PROFESSION.Count(x => !x.isDeleted);
+        }
+
+        public bool Delete(int id)
+        {
+            var entity = _db.PROFESSION.First(x => x.professionID == id);
+            entity.isDeleted = true;
+            entity.deletedDate = DateTime.Today;
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool Exists(string name)
+        {
+            return _db.PROFESSION.Any(x => !x.isDeleted && x.profession1 == name);
+        }
+
+        public List<Profession> GetAll()
+        {
+            var data = _db.PROFESSION
+                .Where(x => !x.isDeleted)
+                .OrderBy(x => x.profession1)
+                .ToList();
+
+            return data
+                .Select(x => Profession.Rehydrate(
+                    x.professionID,
+                    x.profession1
+                ))
+                .ToList();
+        }
+
+        public bool GetBack(int id)
+        {
+            var entity = _db.PROFESSION.First(x => x.professionID == id);
+            entity.isDeleted = false;
+            entity.deletedDate = null;
+            _db.SaveChanges();
+            return true;
+        }
+
+        public Profession GetById(int id)
+        {
+            var entity = _db.PROFESSION.FirstOrDefault(x => x.professionID == id && !x.isDeleted);
+            if (entity == null) return null;
+
+            var profession = new Profession(entity.profession1);
+            profession.SetId(entity.professionID);
+            return profession;
+        }
+
+        public bool Insert(Profession profession)
+        {
+            _db.PROFESSION.Add(new PROFESSION
+            {
+                profession1 = profession.ProfessionName
+            });
+
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool PermanentDelete(int id)
+        {
+            var entity = _db.PROFESSION.FirstOrDefault(x => x.professionID == id);
+
+            if (entity == null)
+                return false;
+
+            _db.PROFESSION.Remove(entity);
+            _db.SaveChanges();
+
+            return true;
+        }
+
+        public bool Update(Profession profession)
+        {
+            var entity = _db.PROFESSION.First(x => x.professionID == profession.ProfessionId);
+            entity.profession1 = profession.ProfessionName;
+            _db.SaveChanges();
+            return true;
+        }
+    }
+}
