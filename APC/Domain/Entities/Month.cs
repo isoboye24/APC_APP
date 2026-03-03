bb538c76ac6e882ffdde652d65a021d8d9a6ad4c
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-03 15:29:44 +0100

    Created month entity

---
+++ APC/Domain/Entities/Month.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Entities
+{
+    public class Month
+    {
+        public int MonthId { get; private set; }
+        public string Name { get; private set; }
+
+        public Month(string name)
+        {
+            Name = name;
+        }
+
+        public static Month Rehydrate(int id, string name)
+        {
+            return new Month(name)
+            {
+                MonthId = id
+            };
+        }
+    }
+}
