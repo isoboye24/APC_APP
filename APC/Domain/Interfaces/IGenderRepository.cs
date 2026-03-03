855daa5c4fb5f87aa74f2a86a3bd6134d6d818a8
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-03 15:58:29 +0100

    Created gender repository

---
+++ APC/Domain/Interfaces/IGenderRepository.cs
@@ -0,0 +1,15 @@
+﻿using APC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Interfaces
+{
+    public interface IGenderRepository
+    {
+        List<Gender> GetAll();
+        Gender GetById(int id);
+    }
+}
---
+++ APC/Infrastructure/Repositories/GenderRepository.cs
@@ -0,0 +1,44 @@
+﻿using APC.DAL;
+using APC.Domain.Entities;
+using APC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Infrastructure.Repositories
+{
+    public class GenderRepository : IGenderRepository
+    {
+        private readonly APCEntities _db;
+        public GenderRepository(APCEntities db)
+        {
+            _db = db;
+        }
+
+        public List<Gender> GetAll()
+        {
+            var data = _db.GENDER
+                .OrderBy(x => x.genderID)
+                .ToList();
+
+            return data
+                .Select(x => Gender.Rehydrate(
+                    x.genderID,
+                    x.genderName
+                ))
+                .ToList();
+        }
+
+        public Gender GetById(int id)
+        {
+            var entity = _db.GENDER.FirstOrDefault(x => x.genderID == id);
+            if (entity == null) return null;
+
+            var gender = new Gender(entity.genderName);
+            gender.SetId(entity.genderID);
+            return gender;
+        }
+    }
+}
