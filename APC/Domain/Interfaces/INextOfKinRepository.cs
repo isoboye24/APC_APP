1b1d647b83482b408bc8bcf5a0e553710c2ea52d
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-04 21:55:46 +0100

    Next of kin repository created

---
+++ APC/Domain/Interfaces/INextOfKinRepository.cs
@@ -0,0 +1,15 @@
+﻿using APC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Interfaces
+{
+    public interface INextOfKinRepository
+    {
+        List<NextOfKin> GetAll();
+        NextOfKin GetById(int id);
+    }
+}
---
+++ APC/Infrastructure/Repositories/NextOfKinRepository.cs
@@ -0,0 +1,44 @@
+﻿using APC.DAL;
+using APC.Domain.Entities;
+using APC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Infrastructure.Repositories
+{
+    public class NextOfKinRepository : INextOfKinRepository
+    {
+        private readonly APCEntities _db;
+        public NextOfKinRepository(APCEntities db)
+        {
+            _db = db;
+        }
+
+        public List<NextOfKin> GetAll()
+        {
+            var data = _db.NEXT_OF_KIN_RELATIONSHIP
+                .OrderBy(x => x.RelationshipToKinID)
+                .ToList();
+
+            return data
+                .Select(x => NextOfKin.Rehydrate(
+                    x.RelationshipToKinID,
+                    x.RelationshipToKin
+                ))
+                .ToList();
+        }
+
+        public NextOfKin GetById(int id)
+        {
+            var entity = _db.NEXT_OF_KIN_RELATIONSHIP.FirstOrDefault(x => x.RelationshipToKinID == id);
+            if (entity == null) return null;
+
+            var nextOfKinRel = new NextOfKin(entity.RelationshipToKin);
+            nextOfKinRel.SetId(entity.RelationshipToKinID);
+            return nextOfKinRel;
+        }
+    }
+}
