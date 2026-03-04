2ac3cae43294747ff0b67ffa9352d6b3da7be9f4
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-04 15:20:03 +0100

    Created Membership status repository

---
+++ APC/Infrastructure/Repositories/MembershipStatusRepository.cs
@@ -0,0 +1,45 @@
+﻿using APC.DAL;
+using APC.Domain.Entities;
+using APC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Infrastructure.Repositories
+{
+    public class MembershipStatusRepository : IMembershipStatusRepository
+    {
+        private readonly APCEntities _db;
+        public MembershipStatusRepository(APCEntities db)
+        {
+            _db = db;
+        }
+
+        public List<MembershipStatus> GetAll()
+        {
+            var data = _db.MEMBERSHIP_STATUS
+                .OrderBy(x => x.membershipStatusID)
+                .ToList();
+
+            return data
+                .Select(x => MembershipStatus.Rehydrate(
+                    x.membershipStatusID,
+                    x.membershipStatus
+                ))
+                .ToList();
+        }
+
+        public MembershipStatus GetById(int id)
+        {
+            var entity = _db.MEMBERSHIP_STATUS.FirstOrDefault(x => x.membershipStatusID == id);
+            if (entity == null) return null;
+
+            var status = new MembershipStatus(entity.membershipStatus);
+            status.SetId(entity.membershipStatusID);
+            return status;
+        }
+
+    }
+}
