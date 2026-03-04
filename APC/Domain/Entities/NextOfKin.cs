21c08f0f00c9d43cf25952c2065118b651638916
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-04 21:49:42 +0100

    Created next of kin entity.

---
+++ APC/Domain/Entities/NextOfKin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Entities
+{
+    public class NextOfKin
+    {
+        public int NextOfKinId { get; private set; }
+        public string NextOfKinName { get; private set; }
+
+        public NextOfKin(string name)
+        {
+            NextOfKinName = name;
+        }
+
+        public static NextOfKin Rehydrate(int id, string name)
+        {
+            return new NextOfKin(name)
+            {
+                NextOfKinId = id
+            };
+        }
+
+        public void SetId(int id)
+        {
+            NextOfKinId = id;
+        }
+    }
+}
