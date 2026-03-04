95f5d27ffd8fe4cb0bbbc3e1ab68a1642cab92c3
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-04 15:43:52 +0100

    Created payment status repository

---
+++ APC/Domain/Interfaces/IPaymentStatusRepository.cs
@@ -0,0 +1,22 @@
+﻿using APC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Interfaces
+{
+    public interface IPaymentStatusRepository
+    {
+        List<PaymentStatus> GetAll();
+        PaymentStatus GetById(int id);
+        bool Insert(PaymentStatus status);
+        bool Update(PaymentStatus status);
+        bool Delete(int id);
+        bool GetBack(int id);
+        bool PermanentDelete(int id);
+        bool Exists(string name);
+        int Count();
+    }
+}
---
+++ APC/Infrastructure/Repositories/PaymentStatusRepository.cs
@@ -0,0 +1,105 @@
+﻿using APC.DAL;
+using APC.Domain.Entities;
+using APC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Infrastructure.Repositories
+{
+    public class PaymentStatusRepository : IPaymentStatusRepository
+    {
+        private readonly APCEntities _db;
+        public PaymentStatusRepository(APCEntities db)
+        {
+            _db = db;
+        }
+
+        public int Count()
+        {
+            return _db.PAYMENT_STATUS.Count(x => !x.isDeleted);
+        }
+
+        public bool Delete(int id)
+        {
+            var entity = _db.PAYMENT_STATUS.First(x => x.paymentStatusID == id);
+            entity.isDeleted = true;
+            entity.deletedDate = DateTime.Today;
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool Exists(string name)
+        {
+            return _db.PAYMENT_STATUS.Any(x => !x.isDeleted && x.paymentStatusName == name);
+        }
+
+        public List<PaymentStatus> GetAll()
+        {
+            var data = _db.PAYMENT_STATUS
+                .Where(x => !x.isDeleted)
+                .OrderBy(x => x.paymentStatusName)
+                .ToList();
+
+            return data
+                .Select(x => PaymentStatus.Rehydrate(
+                    x.paymentStatusID,
+                    x.paymentStatusName
+                ))
+                .ToList();
+        }
+
+        public bool GetBack(int id)
+        {
+            var entity = _db.PAYMENT_STATUS.First(x => x.paymentStatusID == id);
+            entity.isDeleted = false;
+            entity.deletedDate = null;
+            _db.SaveChanges();
+            return true;
+        }
+
+        public PaymentStatus GetById(int id)
+        {
+            var entity = _db.PAYMENT_STATUS.FirstOrDefault(x => x.paymentStatusID == id && !x.isDeleted);
+            if (entity == null) return null;
+
+            var paymentStatus = new PaymentStatus(entity.paymentStatusName);
+            paymentStatus.SetId(entity.paymentStatusID);
+            return paymentStatus;
+        }
+
+        public bool Insert(PaymentStatus status)
+        {
+            _db.PAYMENT_STATUS.Add(new PAYMENT_STATUS
+            {
+                paymentStatusName = status.PaymentStatusName
+            });
+
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool PermanentDelete(int id)
+        {
+            var entity = _db.PAYMENT_STATUS.FirstOrDefault(x => x.paymentStatusID == id);
+
+            if (entity == null)
+                return false;
+
+            _db.PAYMENT_STATUS.Remove(entity);
+            _db.SaveChanges();
+
+            return true;
+        }
+
+        public bool Update(PaymentStatus status)
+        {
+            var entity = _db.PAYMENT_STATUS.First(x => x.paymentStatusID == status.PaymentStatusId);
+            entity.paymentStatusName = status.PaymentStatusName;
+            _db.SaveChanges();
+            return true;
+        }
+    }
+}
