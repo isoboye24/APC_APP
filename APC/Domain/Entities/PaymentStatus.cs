246bcfb7e78bc7ee7341a3ca93557396b83d32be
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-04 15:34:00 +0100

    Created the payment status entity

---
+++ APC/Domain/Entities/PaymentStatus.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Entities
+{
+    public class PaymentStatus
+    {
+        public int PaymentStatusId { get; private set; }
+        public string PaymentStatusName { get; private set; }
+
+
+        public PaymentStatus(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Payment status name cannot be empty");
+
+            PaymentStatusName = name.Trim();
+        }
+
+        public static PaymentStatus Rehydrate(int id, string name)
+        {
+            return new PaymentStatus(name)
+            {
+                PaymentStatusId = id,
+            };
+        }
+
+        public void UpdateName(string newName)
+        {
+            if (string.IsNullOrWhiteSpace(newName))
+                throw new ArgumentException("Payment status name cannot be empty");
+
+            PaymentStatusName = newName.Trim();
+        }
+
+        public void SetId(int id)
+        {
+            PaymentStatusId = id;
+        }
+    }
+}
