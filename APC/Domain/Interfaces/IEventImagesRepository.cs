3b1083afe8b1b24aebca8b60a626440a17be0ccc
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-03-05 16:54:44 +0100

    Event images repository created.

---
+++ APC/Domain/Interfaces/IEventImagesRepository.cs
@@ -0,0 +1,22 @@
+﻿using APC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Domain.Interfaces
+{
+    public interface IEventImagesRepository
+    {
+        List<EventImages> GetAll();
+        EventImages GetById(int id);
+        bool Insert(EventImages data);
+        bool Update(EventImages data);
+        bool Delete(int id);
+        bool GetBack(int id);
+        bool PermanentDelete(int id);
+        bool Exists(int eventId, string imagePath);
+        int Count();
+    }
+}
---
+++ APC/Infrastructure/Repositories/EventImagesRepository.cs
@@ -0,0 +1,133 @@
+﻿using APC.DAL;
+using APC.Domain.Entities;
+using APC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APC.Infrastructure.Repositories
+{
+    public class EventImagesRepository : IEventImagesRepository
+    {
+        private readonly APCEntities _db;
+        public EventImagesRepository(APCEntities db)
+        {
+            _db = db;
+        }
+
+        public int Count()
+        {
+            return _db.EVENT_IMAGE.Count(x => !x.isDeleted);
+        }
+
+        public bool Delete(int id)
+        {
+            var entity = _db.EVENT_IMAGE.First(x => x.eventImageID == id);
+            entity.isDeleted = true;
+            entity.deletedDate = DateTime.Today;
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool Exists(int eventId, string imagePath)
+        {
+            return _db.EVENT_IMAGE.Any(x => !x.isDeleted && x.eventID == eventId && x.imagePath == imagePath);
+        }
+
+        public List<EventImages> GetAll()
+        {
+            var data = _db.EVENT_IMAGE
+                .Where(x => !x.isDeleted)
+                .OrderBy(x => x.imageCaption)
+                .ToList();
+
+            return data
+                .Select(x => EventImages.Rehydrate(
+                    x.eventImageID,
+                    x.eventID,
+                    x.summary,
+                    x.imagePath,
+                    x.imageCaption
+                ))
+                .ToList();
+        }
+
+        public bool GetBack(int id)
+        {
+            var entity = _db.EVENT_IMAGE.First(x => x.eventImageID == id);
+            entity.isDeleted = false;
+            entity.deletedDate = null;
+            _db.SaveChanges();
+            return true;
+        }
+
+        public EventImages GetById(int id)
+        {
+            var entity = _db.EVENT_IMAGE
+                .Where(x => x.eventImageID == id && !x.isDeleted)
+                .Select(x => new
+                {
+                    x.eventImageID,
+                    x.eventID,
+                    x.summary,
+                    x.imagePath,
+                    x.imageCaption
+                })
+                .FirstOrDefault();
+
+            if (entity == null)
+                return null;
+
+            return EventImages.Rehydrate(
+                    entity.eventImageID,
+                    entity.eventID,
+                    entity.summary,
+                    entity.imagePath,
+                    entity.imageCaption
+            );
+        }
+
+        public bool Insert(EventImages data)
+        {
+            _db.EVENT_IMAGE.Add(new EVENT_IMAGE
+            {
+                eventID = data.EventId,
+                summary = data.Summary,
+                imagePath = data.ImagePath,
+                imageCaption = data.ImageCaption,
+                
+            });
+
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool PermanentDelete(int id)
+        {
+            var entity = _db.EVENT_IMAGE.FirstOrDefault(x => x.eventImageID == id);
+
+            if (entity == null)
+                return false;
+
+            _db.EVENT_IMAGE.Remove(entity);
+            _db.SaveChanges();
+
+            return true;
+        }
+
+        public bool Update(EventImages data)
+        {
+            var entity = _db.EVENT_IMAGE.First(x => x.eventImageID == data.EventImagesId);
+            entity.eventID = data.EventId;
+            entity.summary = data.Summary;
+            entity.imagePath = data.ImagePath;
+            entity.imageCaption = data.ImageCaption;
+
+            _db.SaveChanges();
+            return true;
+        }
+    }
+}
