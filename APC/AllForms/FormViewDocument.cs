2f703c68362b128b643d7c7e20efda18aa294e83
Dan-Obu Isoboye Vincent <isobarguy4real@gmail.com>
2026-05-30 11:37:20 +0200

    FormViewDocument updated

--- APC/AllForms/FormViewDocument.cs
+++ APC/AllForms/FormViewDocument.cs
@@ -1,15 +1,7 @@
-﻿using APC.DAL.DTO;
+﻿using APC.Helper;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace APC.AllForms
 {
@@ -41,20 +33,25 @@ namespace APC.AllForms
 
         private void FormViewDocument_Load(object sender, EventArgs e)
         {
-            labelWordsCount.Font = new Font("Segoe UI", 11, FontStyle.Regular);
-            label1.Font = new Font("Segoe UI", 11, FontStyle.Regular);
-            btnClose.Font = new Font("Segoe UI", 14, FontStyle.Bold);
-
             labelWordsCount.Hide();
-            this.Text = detail.DocumentName +" (" + detail.Date + ")";
-            string filePath = System.Windows.Forms.Application.StartupPath + "\\documents\\" + detail.DocumentPath;
-            if (detail.DocumentType == "Word Document")
+            this.Text = _documentDTO.DocumentName +" (" + _documentDTO.FormattedDate + ")";
+            string filePath = System.Windows.Forms.Application.StartupPath + "\\documents\\" + _documentDTO.DocumentPath;
+            if (_documentDTO.DocumentType == "Word Document")
             {
                 ReadFiles.ReadWord(filePath, txtDocumentContent);
                 string[] words = txtDocumentContent.Text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
                 labelWordsCount.Text = words.Length.ToString();
                 labelWordsCount.Visible = true;
-            }            
-        }        
+            }
+
+            ControlsFont();
+        }
+
+        private void ControlsFont()
+        {
+            GeneralHelper.ApplyBoldFont(14, label1, btnClose);
+            GeneralHelper.ApplyRegularFont(16, txtDocumentContent);
+            GeneralHelper.ApplyRegularFont(11, labelWordsCount);
+        }
     }
 }
